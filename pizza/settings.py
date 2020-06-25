97dba82a90c7432c118ba0570887b8bb3365fedf
Shanaka <yased27@icloud.com>
2020-06-25 17:03:56 +1000

    whitenoise.django.GzipManifestStaticFilesStorage

--- pizza/settings.py
+++ pizza/settings.py
@@ -115,11 +115,16 @@ USE_L10N = True
 USE_TZ = True
 
 
+MIDDLEWARE_CLASSES = (
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
