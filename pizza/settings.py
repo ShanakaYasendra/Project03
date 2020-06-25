4f0be36587592df427173ea90590bffd67881d19
Shanaka <yased27@icloud.com>
2020-06-25 17:54:33 +1000

    static

--- pizza/settings.py
+++ pizza/settings.py
@@ -127,8 +127,8 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 
 STATICFILES_DIRS = [
-    ('css', 'css'),
-    ('image', 'image'),
+    ('css', 'static/css'),
+    ('image', 'static/image'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
