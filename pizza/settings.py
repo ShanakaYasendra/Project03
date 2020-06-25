b811e01ea6b22240bbc89931a4b4a19be121f39b
Shanaka <yased27@icloud.com>
2020-06-25 17:22:18 +1000

    static

--- pizza/settings.py
+++ pizza/settings.py
@@ -122,13 +122,13 @@ MIDDLEWARE = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
