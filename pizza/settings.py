c201f35675a4001f457b49aad24c5d49d45b49d8
Shanaka <yased27@icloud.com>
2020-06-25 17:19:57 +1000

    static

--- pizza/settings.py
+++ pizza/settings.py
@@ -126,7 +126,7 @@ MIDDLEWARE = [
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
