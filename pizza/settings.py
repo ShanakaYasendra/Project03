ba4cda2e6d3535594260d8ff89b519ef19a55ce5
Shanaka <yased27@icloud.com>
2020-06-25 17:17:48 +1000

    change the storage

--- pizza/settings.py
+++ pizza/settings.py
@@ -130,5 +130,5 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
